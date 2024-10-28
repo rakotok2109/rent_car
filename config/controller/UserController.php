d03f87621df72547b16e17d6e8614f84367aa24b
arakot <nyarovantsoanathan.rakotonoera@eemi.com>
2024-10-28 14:06:50 +0100

    Initialisation de la fonction login by Nathan

--- config/controller/UserController.php
+++ config/controller/UserController.php
@@ -1,13 +1,18 @@
 <?php 
 
-// require_once(__DIR__ . /config/init.php)
+require_once('../config/init.php');
 
 class UserController {
-    public static function subscribe (User $user)
-    {
-        $password = password_hash($user->getPassword(), PASSWORD_DEFAULT);
-
+    public static function login(User $user) {
         $pdo = PDOUtils::getSharedInstance();
-        $pdo->execSQL('INSERT INTO users (email, password, role) VALUES (?, ?, ?)', [$user->getEmail(), $password, $user->getRole()]);
+        $result = $pdo->requestSQL('SELECT * FROM users WHERE password = ?', [$password]);
+        if ($result) {
+            $_SESSION['user_id'] = $row['id'];
+            $_SESSION['email'] = $email;
+            header("Location: ../../pages/profil.php");
+            exit();
+        } else {
+            echo "Combinaison email et mot de passe incorrect.";
+        }
     }
 }

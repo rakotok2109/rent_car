de78bf45041024f92ba323945a7846cb91ce1ca4
arakot <nyarovantsoanathan.rakotonoera@eemi.com>
2024-10-28 14:33:32 +0100

    Initialisation de la vérification lors de la connexion by Nathan

--- pages/verif.php
+++ pages/verif.php
@@ -9,16 +9,14 @@ if($_GET['id'] == 'subscribe') {
    
     // header('Location: /');
 }
-// else if($_GET['id'] == 'login') {
-//     $user = UserController::login($_POST['email'], $_POST['password']);
-//     if($user) {
-//         $_SESSION['user'] = $user;
-//         header('Location: /');
-//     }
-//     else {
-//         header('Location: /?error=1');
-//     }
-// }
+
+else if($_GET['id'] == 'login') {
+
+    UserController::login($user);
+    
+}
+
+
 // else if($_GET['id'] == 'logout') {
 //     unset($_SESSION['user']);
 //     header('Location: /');

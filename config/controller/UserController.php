d4f9adfff64616eef9bc757b56b1a9a0894acd94
arakot <nyarovantsoanathan.rakotonoera@eemi.com>
2024-10-28 14:13:22 +0100

    Modification de la fonction login + création de la vue en html by Nathan

--- config/controller/UserController.php
+++ config/controller/UserController.php
@@ -4,6 +4,9 @@ require_once('../config/init.php');
 
 class UserController {
     public static function login(User $user) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
         $pdo = PDOUtils::getSharedInstance();
         $result = $pdo->requestSQL('SELECT * FROM users WHERE password = ?', [$password]);
         if ($result) {

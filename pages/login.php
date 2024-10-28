f99ab7155287262bb308ba2ee496af6b63affbce
arakot <nyarovantsoanathan.rakotonoera@eemi.com>
2024-10-28 14:14:36 +0100

    Création de la vue en html pour la connexion utilisateur by Nathan

---
+++ pages/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+<body>
+    <div class="form-container">
+        <form action="../config/controller/UserController.php" method="POST">
+            <label for="email"></label>
+            <input type="email" id="email" name="email"  placeholder="Email">
+        
+            <label for="mot_de_passe"></label>
+            <input type="password" id="password" name="password"  placeholder="Mot de passe"><br /></br />
+            
+            <input type="submit" value="Se connecter">
+        </form>
+    </div>
+</body>
+</body>
+</html>
